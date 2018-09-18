69832bc25c42923c80111a0c72e4cb799b081a88
Shahed Bin Satter <shahed.satter@datasoft-bd.com>
2018-09-18 12:49:15 +0600

    Starter project with Spring boot along with security, web

---
+++ src/main/java/com/sbsatter/projectreview/ProjectReviewApplication.java
@@ -0,0 +1,12 @@
+package com.sbsatter.projectreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectReviewApplication.class, args);
+	}
+}
